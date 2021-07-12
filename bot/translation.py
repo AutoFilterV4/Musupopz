e4af3ff1b2490702cbe03bad068846dec44f6c08
AutoFilterV4 <87136279+AutoFilterV4@users.noreply.github.com>
2021-07-12 06:44:45 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -11,7 +11,7 @@ Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥�
 
 For More Details Click Help Button Below..
 
-@M_STER_TECH
+@punisherofc
 </i>"""    
     
     HELP_TEXT = """
@@ -44,10 +44,10 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@M_STER_TECH
+@punisherofc
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> LUII</code>
     
-<b>➥ Creator</b> : @CYBER_YT1
+<b>➥ Creator</b> : @punisherofc
 """
